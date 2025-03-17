5a5c057b0ccf3289645f73bc3157a94acf75a0d9
Sam Yao <turisesonia@gmail.com>
2025-03-17 14:37:45 +0800

    refactor: remove unused mixins from __all__ in flex_alchemy

--- src/flex_alchemy/__init__.py
+++ src/flex_alchemy/__init__.py
@@ -1,9 +1,3 @@
 from .activerecord import ActiveRecord
-from .mixins.timestamp import TimestampMixin
-from .mixins.softdelete import SoftDeleteMixin
 
-__all__ = [
-    "ActiveRecord",
-    "TimestampMixin",
-    "SoftDeleteMixin",
-]
+__all__ = ["ActiveRecord"]
